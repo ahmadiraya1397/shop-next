bf31f47cf5e5b048f5c1600a5c699d31120150cc
abdollah <ahmadi.raya@gmail.com>
2022-11-23 00:31:49 +0330

    Practice: Design Sign in Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {Provider} from "react-redux";
 import {store} from './../app/store'
+// add bootstrap css
+import 'bootstrap/dist/css/bootstrap.css'
+
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
     <Component {...pageProps}/>
